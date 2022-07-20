5544c48e31edcb452052c533f71831361ba3493c
GopakumarPK7 <109574705+GopakumarPK7@users.noreply.github.com>
2022-07-20 10:14:21 +0530

    Update PracticeMaven.java

--- SamplaeMavenProject/src/test/java/org/mavensample/project/PracticeMaven.java
+++ SamplaeMavenProject/src/test/java/org/mavensample/project/PracticeMaven.java
@@ -13,7 +13,7 @@ System.out.println("TC -");
 public void practiceMaven1()
 {
 System.out.println("TC - 2");
-System.out.println("TC passed");  
+System.out.println("TC Failed");  
 }
 @Test
 public void practiceMaven2()
